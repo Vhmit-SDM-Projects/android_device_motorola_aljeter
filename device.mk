d8acc54f9851f4a63d372ebd877954b1f8e78696
Vhmit <praxedes.9630@gmail.com>
2023-05-17 17:39:39 -0300

    jeter: Import power_profile from jeter
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- device.mk
+++ device.mk
@@ -9,5 +9,9 @@ DEVICE_PATH := device/motorola/jeter
 # Inherit from msm8937-common
 $(call inherit-product, device/motorola/msm8937-common/msm8937.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/jeter/jeter-vendor.mk)
