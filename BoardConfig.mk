7dcd13ef117a19f6c5c31bf518deeacd971510b6
Vhmit <praxedes.9630@gmail.com>
2023-05-23 14:55:14 -0300

    aljeter: Rebrand jeter for aljeter
    * Set screen density for 300 = 384 in system
    
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,13 +7,13 @@
 # Inherit from msm8937-common
 -include device/motorola/msm8937-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/motorola/jeter
+DEVICE_PATH := device/motorola/aljeter
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := jeter,aljeter
+TARGET_OTA_ASSERT_DEVICE := aljeter
 
 # Display
-TARGET_SCREEN_DENSITY := 320
+TARGET_SCREEN_DENSITY := 300
 
 # Hals
 TARGET_QCOM_AUDIO_VARIANT := caf-msm8996
@@ -24,7 +24,7 @@ TARGET_QCOM_DISPLAY_VARIANT := caf-msm8996
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := jeter_defconfig
+TARGET_KERNEL_CONFIG := aljeter_defconfig
 
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
@@ -41,4 +41,4 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from the proprietary version
-include vendor/motorola/jeter/BoardConfigVendor.mk
+include vendor/motorola/aljeter/BoardConfigVendor.mk
